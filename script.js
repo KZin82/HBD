91d018d6ffffda6f84f099493ee942e31b7f6eac
KZin82 <phyokzp1993@gmail.com>
2025-08-06 11:49:29 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 window.checkPassword = function () {
   const pw = document.getElementById("password").value;
-  if (pw === "2191994")
+  if (pw === "2191994") {
     document.getElementById("loginScreen").style.display = "none";
     document.getElementById("cakeScreen").classList.remove("hidden");
-  } 
+  }
+} 
 function nextStep(){
     document.getElementById("cakeScreen").classList.add("hidden");
     document.getElementById("letterScreen").classList.remove("hidden");
